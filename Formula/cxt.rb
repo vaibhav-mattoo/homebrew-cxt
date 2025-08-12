299d3d1cb9390852b48d48068ffa3c4d1ac535c9
vaibhav <vaibhavmattoo1@gmail.com>
2025-08-11 20:44:27 -0500

    bumped to v0.1.6

--- Formula/cxt.rb
+++ Formula/cxt.rb
@@ -1,8 +1,8 @@
 class Cxt < Formula
   desc "A command-line tool that aggregates file and directory contents into your clipboard, perfect for providing project context to AI chatbots"
   homepage "https://github.com/vaibhav-mattoo/cxt"
-  url "https://github.com/vaibhav-mattoo/cxt/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "cf04dcd4c6b93727961b501b3fca42befa9c2351afd01dcdc69d3ea5764a3058"
+  url "https://github.com/vaibhav-mattoo/cxt/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "e01cd8322f3774ae15a9311cbcb28b951747b2c91945a6cbc1087cae9c5f1f4a"
   license "MIT"
 
   depends_on "rust" => :build
